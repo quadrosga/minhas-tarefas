ea4cee0e7bdb3904fb207af7d9a0bbd049669608
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-11-23 14:27:45 -0300

    config filtro por prioridade e status

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../styles/components/FiltroCard'
 
 import * as S from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+
 import { RootReducer } from '../../store'
 import { alterarTermo } from '../../store/reducers/filtro'
 
@@ -19,12 +21,32 @@ const BarraLateral = () => {
           onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
         />
         <S.Filtros>
-          <FiltroCard legenda="pendentes" contador={1} />
-          <FiltroCard legenda="concluídas" contador={2} />
-          <FiltroCard legenda="urgentes" contador={3} />
-          <FiltroCard legenda="importantes" contador={4} />
-          <FiltroCard legenda="normal" contador={5} />
-          <FiltroCard legenda="todas" contador={10} />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="concluídas"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            legenda="importantes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="normal"
+          />
+          <FiltroCard criterio="todas" legenda="todas" />
         </S.Filtros>
       </div>
     </S.Aside>
