876400386f473e4c37c198c3f6d89b4fdb29ca84
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-11-23 16:20:41 -0300

    configurando navegação entre páginas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import BarraLateral from '../../containers/BarraLateral'
 import ListaDeTarefas from '../../containers/ListaDeTarefas'
+import BotaoAdicionar from '../../styles/components/BotaoAdicionar'
 
 const Home = () => (
   <>
     <BarraLateral />
     <ListaDeTarefas />
+    <BotaoAdicionar />
   </>
 )
 
